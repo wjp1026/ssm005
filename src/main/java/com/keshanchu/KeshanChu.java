d7d773c8b9d2d6269de9321007fed6ae4690c232
wujinpeng <3197662050@qq.com>
2022-11-17 17:23:28 +0800

    branch001提交01

--- src/main/java/com/keshanchu/KeshanChu.java
+++ src/main/java/com/keshanchu/KeshanChu.java
@@ -10,5 +10,9 @@ public class KeshanChu {
     public void aa(){
         System.out.println("这是修改了一次的");
         System.out.println("这是第二次修改");
+        System.out.println("master主干分支添加的--------------");
+        System.out.println("branch001分支添加的-==============");
+
+        System.out.println("branch001第二次添加且提交");
     }
 }
